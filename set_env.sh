14822fab63c80a528c83b48d9d404ed40efa522c
Rasterayzer <92667149+Rasterayzer@users.noreply.github.com>
2021-11-17 15:50:53 +0100

    Update set_env script

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-setx POSTGRES_USERNAME trouvinez
-setx POSTGRES_PASSWORD Apteryx02
-setx POSTGRES_HOST cdr.cole4cytpi1a.us-east-1.rds.amazonaws.com
-setx POSTGRES_DB cdr
-setx AWS_BUCKET arn:aws:s3:::udagram-p3-209343855061-dev
-setx AWS_REGION us-east-1
-setx AWS_PROFILE default
-setx JWT_SECRET p3-project
-setx URL http://localhost:8100
+export $POSTGRES_USERNAME=trouvinez
+export $POSTGRES_PASSWORD=Apteryx02
+export $POSTGRES_HOST=cdr.cole4cytpi1a.us-east-1.rds.amazonaws.com
+export $POSTGRES_DB=cdr
+export $AWS_BUCKET=arn:aws:s3:::udagram-p3-209343855061-dev
+export $AWS_REGION=us-east-1
+export $AWS_PROFILE=default
+export $JWT_SECRET=p3-project
+export $URL=http://localhost:8100
