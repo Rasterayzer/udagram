b0c3de527caae697bdb24f743e00e31d0c9d4e59
Rasterayzer <92667149+Rasterayzer@users.noreply.github.com>
2021-11-17 15:46:19 +0100

    Add updated starter content

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+setx POSTGRES_USERNAME trouvinez
+setx POSTGRES_PASSWORD Apteryx02
+setx POSTGRES_HOST cdr.cole4cytpi1a.us-east-1.rds.amazonaws.com
+setx POSTGRES_DB cdr
+setx AWS_BUCKET arn:aws:s3:::udagram-p3-209343855061-dev
+setx AWS_REGION us-east-1
+setx AWS_PROFILE default
+setx JWT_SECRET p3-project
+setx URL http://localhost:8100
